7bf1c572a07414f8ede3b2c40822c95d44662d61
Marc C <m.ciborowski@hotmail.com>
2020-05-17 18:37:15 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,6 +1,6 @@
 def greeting(name)
+  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
   puts "Please enter your name so that we can greet you more personally:"
   gets.strip
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
 greeting(name)
